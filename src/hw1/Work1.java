275071c59680a7ba418a32cba9662435f9776d7e
Zack <sgps41106@gmail.com>
2023-10-18 18:28:22 +0800

    modified test to test

--- src/hw1/Work1.java
+++ src/hw1/Work1.java
@@ -57,7 +57,7 @@ public class Work1 {
 		System.out.println(5 + '5'); // 數值 + char = 數值,'5'轉成數值 = 53
 		System.out.println(5 + "5"); // 數值串接String
 		
-		System.out.println("sss");
+		System.out.println("test");
 		System.out.println("ddd");
 		System.out.println("ggg");
 }
