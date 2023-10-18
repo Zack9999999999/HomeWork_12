9a464cf84d088c18096afd066c65c62b70cfc2e8
Zack <sgps41106@gmail.com>
2023-10-18 16:14:51 +0800

    First commit

---
+++ src/hw1/Work1.java
@@ -0,0 +1,60 @@
+package hw1;
+
+public class Work1 {
+
+	public static void main(String[] args) {
+//		1.請設計一隻Java程式,計算12,6這兩個數值的和與積
+		
+		int i1 = 12;
+		int i2 = 6;
+		System.out.println(i1 + i2);
+		System.out.println(i1 * i2);
+		
+//		2.請設計一隻Java程式,計算200顆蛋共是幾打幾顆? (一打為12顆)
+		
+		int egg = 200; //蛋
+		int egg12 = egg / 12; //打
+		int egg1 = egg % 12; //顆
+		System.out.println(egg12 + "打" + egg1 + "顆");
+	
+//		3.請由程式算出256559秒為多少天、多少小時、多少分與多少秒
+		int allsec = 256559;
+		int sec = allsec % 60;
+		int min = (allsec / 60) % 60;
+		int hr = (allsec / 3600) % 24;
+		int day = allsec / 86400;
+		
+		System.out.println(day + "天" + hr + "小時" + min + "分" + sec + "秒");
+		
+//		4.請定義一個常數為3.1415(圓周率),並計算半徑為5的圓面積與圓周長
+//半徑 x 半徑 x 3.1415    半徑 x 2 x 3.1415	
+		final float sum = 3.1415f; //圓周率
+		int a = 5; //半徑
+		float b = a * a * sum; //圓面積
+		float c = a * 2 * sum; //圓周長
+		System.out.println("圓面積: " + b);
+		System.out.println("圓周長: " + c);
+
+//		5.某人在銀行存入150萬,銀行利率為2%,如果每年利息都繼續存入銀行,請用程式計算10年後,本
+//		金加利息共有多少錢 (用複利計算,公式請自行google)	
+//本利和 = 1500000 x (1 + 0.02)n次方	
+	
+		int money = 1500000;
+		double r = 0.02;
+		double z = (1 + r);
+		for(int x = 1; x <= 10; x++)
+			 z *= (1 + r);
+		double allmoney = money * z;
+		System.out.println((int)allmoney + "元");
+	
+//		6.請寫一隻程式,利用System.out.println()印出以下三個運算式結果:
+//		5 + 5
+//		5 + ‘5’
+//		5 + “5”
+//		並請用註解各別說明答案的產生原因
+	
+		System.out.println(5 + 5); // 數值相加
+		System.out.println(5 + '5'); // 數值 + char = 數值,'5'轉成數值 = 53
+		System.out.println(5 + "5"); // 數值串接String
+}
+	}
---
+++ src/hw3/Work3.java
@@ -0,0 +1,93 @@
+package hw3;
+
+import java.util.Scanner;
+
+public class Work3 {
+
+	public static void main(String[] args) {
+//		請設計一隻程式,使用者輸入三個數字後,輸出結果會為正三角形、等腰
+//		三角形、其它三角形或不是三角形,如圖示結果:
+//		正三角形:3邊相等    等腰三角形:2邊相等 其他三角形:a + b > c, a + c > b, b + c > a
+//		5 5 5 正三角形	5 8 5 等腰三角形	5 6 7 其他三角形	0 0 0 不是三角形
+//		(提示:Scanner,"三角形成立條件",判斷式if else)
+//		(進階功能:加入直角三角形的判斷)
+//		a平方 +b平方 =c平方
+
+		Scanner sc1 = new Scanner(System.in);
+		System.out.println("請輸入三個數字:");
+		int[] x = new int[3]; 
+		int number = sc1.nextInt();
+		
+		if (sc1.hasNextInt()) {
+			for(int i = 0; i < 3; i++) {
+				x[i] = number;
+				if() {
+					
+				}
+			}
+		} else
+			System.out.println("錯誤");
+
+//================================================================================
+
+//		請設計一隻程式,會亂數產生一個0~9的數字,然後可以玩猜數字遊戲,猜錯會顯示錯誤訊息,猜
+//		對則顯示正確訊息,如圖示結果:
+//
+//		(提示:Scanner,亂數方法,無窮迴圈)
+//		(進階功能:產生0~100亂數,每次猜就會提示你是大於還是小於正確答案)
+
+//		Scanner sc2 = new Scanner(System.in);
+//
+//		System.out.println("開始猜數字吧!");
+//
+//		while (true) {
+//
+//			int number = (int) (Math.random() * 10);
+//
+//			int i = sc2.nextInt();
+//
+//			if (i == number) {
+//				System.out.println("答對了! 答案就是 " + i);
+//				break;
+//			} else
+//				System.out.println("猜錯囉");
+//		}
+
+//      進階		
+//		int number = (int)(Math.random() * 101);
+//		
+//		while(true) {
+//	
+//			int i = sc2.nextInt();
+//			
+//			if(i == number){
+//				System.out.println("答對了! 答案就是 " + i);
+//				break;
+//			}else if(i < number) {
+//				System.out.println("小於正確答案");
+//			}else {
+//				System.out.println("大於正確答案");
+//			}
+//		}
+
+//		阿文很喜歡簽大樂透(1~49),但他是個善變的人,上次討厭數字是4,但這次他想要依心情決定討
+//		厭哪個數字,請您設計一隻程式,讓阿文可以輸入他不想要的數字(1~9),畫面會顯示他可以選擇
+//		的號碼與總數,如圖:
+//
+//		(提示:Scanner)
+//		(進階挑戰:輸入不要的數字後,直接亂數印出6個號碼且不得重複)
+
+//		Scanner sc3 = new Scanner(System.in);
+//
+//		System.out.println("阿文請輸入你討厭哪個數字?");
+
+//		int number = sc3.nextInt();
+//
+//		for (int x = 1; x <= 49; x++) {
+//			if (x == number) {
+//				continue;
+//			} else
+//				System.out.print(x + "\t");
+//		}
+	}
+}
