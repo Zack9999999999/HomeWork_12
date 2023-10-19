2f5f4651c0b819061346845ca47d613295c1b5b1
Zack <sgps41106@gmail.com>
2023-10-19 16:44:56 +0800

    Update method in Work3-1

--- src/hw3/Work3.java
+++ src/hw3/Work3.java
@@ -13,29 +13,17 @@ public class Work3 {
 //		(進階功能:加入直角三角形的判斷)
 //		a平方 +b平方 =c平方
 
-//		Scanner sc1 = new Scanner(System.in);
-//		System.out.println("請輸入三個數字:");
-//
-//		int[] x = new int[3];
-//
-//		for (int i = 0; i < 3; ) {
-//			if (sc1.hasNextInt()) {
-//				x[i] = sc1.nextInt();
-//				i++;
-//			} else
-//				sc1.next();
-//		}
-//		Arrays.sort(x);
-//		if (x[0] == x[1] && x[1] == x[2]) {
-//			System.out.println("正三角形");
-//		} else if (x[0] == x[1] && x[2] > x[1]) {
-//			System.out.println("等腰三角形");
-//		} else if (x[0] < x[1] && x[1] < x[2]) {
-//			System.out.println("其他三角形");
-//		} else if (x[0] * x[0] + x[1] * x[1] == x[2] * x[2]) {
-//			System.out.println("直角三角形");
-//		} else if (x[0] + x[1] <= x[2])
-//			System.out.println("不是三角形");
+		Scanner sc = new Scanner(System.in);
+
+		Triangle tri = new Triangle();
+
+		System.out.println("請輸入三個數字:");
+		
+//1.int[] x 將tri.getThree內得到的陣列儲存起來		
+		int[] x = tri.getThree();
+		
+//2.將int[] x帶入runThreeTime方法內做執行		
+		tri.setThreeTime(x);
 
 //================================================================================
 
@@ -45,39 +33,37 @@ public class Work3 {
 //		(提示:Scanner,亂數方法,無窮迴圈)
 //		(進階功能:產生0~100亂數,每次猜就會提示你是大於還是小於正確答案)
 
-//		Scanner sc2 = new Scanner(System.in);
-//
-//		System.out.println("開始猜數字吧!");
-//
-//		while (true) {
-//
-//			int number = (int) (Math.random() * 10);
-//
-//			int i = sc2.nextInt();
-//
-//			if (i == number) {
-//				System.out.println("答對了! 答案就是 " + i);
-//				break;
-//			} else
-//				System.out.println("猜錯囉");
-//		}
+		System.out.println("開始猜數字吧!");
+
+		while (true) {
+
+			int number = (int) (Math.random() * 10);
+
+			int i = sc.nextInt();
+
+			if (i == number) {
+				System.out.println("答對了! 答案就是 " + i);
+				break;
+			} else
+				System.out.println("猜錯囉");
+		}
 
 //      進階		
-//		int number = (int)(Math.random() * 101);
-//		
-//		while(true) {
-//	
-//			int i = sc2.nextInt();
-//			
-//			if(i == number){
-//				System.out.println("答對了! 答案就是 " + i);
-//				break;
-//			}else if(i < number) {
-//				System.out.println("小於正確答案");
-//			}else {
-//				System.out.println("大於正確答案");
-//			}
-//		}
+		int number = (int) (Math.random() * 101);
+
+		while (true) {
+
+			int i = sc.nextInt();
+
+			if (i == number) {
+				System.out.println("答對了! 答案就是 " + i);
+				break;
+			} else if (i < number) {
+				System.out.println("小於正確答案");
+			} else {
+				System.out.println("大於正確答案");
+			}
+		}
 
 //		阿文很喜歡簽大樂透(1~49),但他是個善變的人,上次討厭數字是4,但這次他想要依心情決定討
 //		厭哪個數字,請您設計一隻程式,讓阿文可以輸入他不想要的數字(1~9),畫面會顯示他可以選擇
@@ -85,18 +71,17 @@ public class Work3 {
 //		(提示:Scanner)
 //		(進階挑戰:輸入不要的數字後,直接亂數印出6個號碼且不得重複)
 
-//		Scanner sc3 = new Scanner(System.in);
-//
-//		System.out.println("阿文請輸入你討厭哪個數字?");
-//
-//		int number = sc3.nextInt();
-//
-//		for (int x = 1; x <= 49; x++) {
-//			if (x % 10 == number || x / 10 % 10 == number) {
-//				continue;
-//			} else
-//				System.out.print(x + " ");
-//		}
-//		System.out.println("總共有35數字可選");
+		System.out.println("阿文請輸入你討厭哪個數字?");
+
+		int number1 = sc.nextInt();
+
+		for (int q = 1; q <= 49; q++) {
+			if (q % 10 == number1 || q / 10 % 10 == number1) {
+				continue;
+			} else
+				System.out.print(q + " ");
+		}
+		System.out.println("\n" + "總共有35數字可選");
+
 	}
 }
