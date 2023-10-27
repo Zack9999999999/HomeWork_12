8d26cb121eada580bd3d0952072d92ff1cd3aec8
Zack <sgps41106@gmail.com>
2023-10-28 01:15:18 +0800

    UPdate Work5

--- src/hw5/Work5.java
+++ src/hw5/Work5.java
@@ -39,36 +39,54 @@ public class Work5 {
 //3.利用Overloading，設計兩個方法int maxElement(int x[][])與double maxElement(double x[][])，
 //可以找出二維陣列的最大值並回傳，如圖：
 
-//	public int maxElement(int x[][]) {
-//		int big = 0;
-//		int k = 1;
-//		for(int i = 0; i < x.length; i++) {
-//			for(int j = 0; j < x[i].length; j++) {
-//					if(x[i][j] < x[i][k]) {
-//						x[i][j] = x[i][k];
-//						k++;
-//					}
-//					big = x[i][j];
-//			}
-//		}
-//		return big;
-//	}
-//	
-	public double maxElement(double x[][]) {
-		double max = 0;
+	public int maxElement(int x[][]) {
 		
+		int max = 0;
+
 		for (int i = 0; i < x.length; i++) {
-			Arrays.sort(x[i]);
+			for (int j = 0; j < x[i].length; j++) {
+				if (x[i][j] > max) {
+					max = x[i][j];
+				}
+			}
+
 		}
-		for(int i = 0; i < x[i].length - 1; i++) {
-			max = x[i][i];
+		return max;
+
+	}
+
+	public double maxElement(double x[][]) {
+
+		double max = 0;
+
+		for (int i = 0; i < x.length; i++) {
+			for (int j = 0; j < x[i].length; j++) {
+				if (x[i][j] > max) {
+					max = x[i][j];
+				}
+			}
+
 		}
 		return max;
 	}
+//排序解法 有空再看怎用
+//	for (int i = 0; i < x.length; i++) {
+//	Arrays.sort(x[i]);
+//}
+//x[i].length - 1 最後一個元素的位置
 
+	
 //5.身為程式設計師的你，收到一個任務，要幫系統的註冊新增驗證碼的功能，請設計一個方法
 //genAuthCode()，當呼叫此方法時，會回傳一個8位數的驗證碼，此驗證碼內容包含了英文大小寫
 //與數字的亂數組合，如圖
+
+	public void getAuthCode() {
+
+//		for (int i = 0; i < 8; i++) {
+//			int ran = (int) (Math.random() * 10);
+//		}
+//		System.out.println("本次隨機產生驗證碼為: " + ran);
+	}
 
 	public static void main(String[] args) {
 
@@ -95,17 +113,11 @@ public class Work5 {
 
 		double[][] doubleArray = { { 1.2, 8.2, 2.2 }, { 7.4, 2.1, 3.5 } };
 
-//		for (int i = 0; i < intArray.length; i++) {
-//			Arrays.sort(intArray[i]);
-//		}
-//		for (int i = 0; i < intArray.length; i++) {
-//			for (int j = 0; j < intArray[i].length; j++) {
-//				System.out.print(intArray[i][j]);
-//			}
-//		}
-
-		double bigOne = w.maxElement(doubleArray);
-		System.out.println(bigOne);
+		int bigOne = w.maxElement(intArray);
+		double bigTwo = w.maxElement(doubleArray);
+		
+		System.out.println("最大值: " + bigOne);
+		System.out.println("最大值: " + bigTwo);
 
 		System.out.println("================================================");
 
