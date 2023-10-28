03ba60f97472f70f1e91ace0609d6381df5689ba
Zack <sgps41106@gmail.com>
2023-10-28 20:55:18 +0800

    Update Work6

--- src/hw6/CalException.java
+++ src/hw6/CalException.java
@@ -1,5 +1,16 @@
 package hw6;
 
-public class CalException {
+public class CalException extends Exception {
 
+	public CalException() {
+
+	}
+
+	public CalException(String msg) {
+		super(msg);
+	}
+
+	public String toString() {
+		return getMessage(); // 返回自訂義的訊息
+	}
 }
--- src/hw6/CalTest.java
+++ src/hw6/CalTest.java
@@ -1,10 +1,40 @@
 package hw6;
 
+import java.util.Scanner;
+
 public class CalTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		CalTest cc = new CalTest();
+		Calculator cal = new Calculator();
+
+		int[] user = new int[2];
+		while (true) { 
+			try {
+				for (int i = 0; i < 2; ) {
+					if (i == 0)
+						System.out.println("請輸入x的值: ");
+					else
+						System.out.println("請輸入y的值: ");
+					
+					if (sc.hasNextInt()) {
+						user[i] = sc.nextInt();
+						i++;
+					} else {
+						System.out.println("輸入格式不正確");
+						sc.next();
+					}
+				}
+				int sum = cal.powerXY(user[0], user[1]);
+				System.out.println(user[0] + "的" + user[1] + "次方等於" + sum);
+				break;
+				
+			} catch (CalException c) {
+				System.out.println(c);
+			}
+		}
 	}
 
 }
