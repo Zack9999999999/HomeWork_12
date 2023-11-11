181b502c6389589e02ce0e4b98762555dee5036e
Zack <sgps41106@gmail.com>
2023-11-12 01:31:30 +0800

    9-2不會卡死了但是感覺怪怪的

--- src/hw9/Money.java
+++ src/hw9/Money.java
@@ -1,16 +1,18 @@
 package hw9;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Money {
 
 	public static void main(String[] args) {
 
 		Bank bank = new Bank();
 
-		BearMom bm = new BearMom(bank);
-		Thread bm1 = new Thread(bm);
+//		BearMom bm = new BearMom(bank);
+		Thread bm1 = new Thread(new BearMom(bank));
 
-		BadBear bb = new BadBear(bank);
-		Thread bb1 = new Thread(bb);
+//		BadBear bb = new BadBear(bank);
+		Thread bb1 = new Thread(new BadBear(bank));
 		bm1.start();
 		bb1.start();
 	}
@@ -25,7 +27,8 @@ class BearMom implements Runnable {
 	}
 
 	public void run() {
-		for (int i = 0; i <= 10; i++) {
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("mom跑了" + i + "次");
 			bank.give(2000);
 		}
 	}
@@ -40,7 +43,8 @@ class BadBear implements Runnable {
 	}
 
 	public void run() {
-		for (int i = 0; i <= 10; i++) {
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("bear跑了" + i + "次");
 			bank.take(1000);
 		}
 	}
@@ -48,33 +52,59 @@ class BadBear implements Runnable {
 
 class Bank {
 
+//	private static final ReentrantLock LOCK = new ReentrantLock();
 	private int money = 0;
+	int count = 0;
 
 	synchronized public void give(int mon) {
 
-		while (money > 3000) {
-			System.out.println("媽媽看到餘額在3000以上,不給了");
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+//		if (count == 10 && money > 3000) {
+//			money += mon;
+//			System.out.println("1.媽媽存了" + mon + ",帳戶共有:" + money);
+//		}
+		if (money > 3000) {
+			System.out.println("1.媽媽看到餘額在3000以上,不給了");
+			notify();
+			System.out.println("1.壞熊被媽媽告知有錢花了!");
 			try {
+				if (count == 10)
+					return;
 				wait();
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
 		money += mon;
-		System.out.println("媽媽存了" + mon + ",帳戶共有:" + money);
-		System.out.println("壞熊被媽媽告知有錢花了!");
-		notify();
+		System.out.println("1.媽媽存了" + mon + ",帳戶共有:" + money);
 	}
 
 	synchronized public void take(int mon) {
-		if (money == 0) {
-			System.out.println("壞熊看到帳戶見底,暫停乞討");
-			notify();
-			System.out.println("媽媽被壞熊乞討!");
 
+		count++;
+//		System.out.println("count = " + count);
+
+		if (count == 10)
+			notify();
+
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
-		while (money < 2000) {
-			System.out.println("壞熊看到餘額在2000以下,開始乞討");
+
+		if (money < 2000) {
+			System.out.println("2.壞熊看到餘額在2000以下,開始乞討");
+		}
+		if (money == 0) {
+			System.out.println("2.壞熊看到帳戶沒錢,暫停領錢");
+			notify();
+			System.out.println("2.媽媽被壞熊乞討!");
 			try {
 				wait();
 			} catch (InterruptedException e) {
@@ -82,8 +112,9 @@ class Bank {
 			}
 		}
 		money -= mon;
-		System.out.println("壞熊領了" + mon + ",帳戶共有:" + money);
-		notify();
-
+		System.out.println("2.壞熊領了" + mon + ",帳戶共有:" + money);
 	}
 }
+
+//次數問題
+//bear跑完10次 導致mom跑到第7次後執行到wait() 卡住
