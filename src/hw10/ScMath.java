c3cfec8497f04c8d1b167baa1d8b94ce87b12e0c
Zack <sgps41106@gmail.com>
2023-11-15 02:58:28 +0800

    ~

--- src/hw10/ScMath.java
+++ src/hw10/ScMath.java
@@ -1,5 +1,7 @@
 package hw10;
 
+import java.text.DecimalFormat;
+import java.text.Format;
 import java.util.Scanner;
 
 //請設計一隻程式，讓使用者可以輸入一個任意數後，可以選擇要以下列何種表示方法顯示
@@ -10,29 +12,45 @@ import java.util.Scanner;
 public class ScMath {
 
 	public static void main(String[] args) {
+
+		double number = 0;
+
 		Scanner sc = new Scanner(System.in);
 		System.out.println("請輸入數字:");
+
 		while (true) {
-			if (sc.hasNextInt()) {
-				int s1 = sc.nextInt();
-				System.out.println(s1);
+			if (sc.hasNextDouble()) {
+				number = sc.nextDouble();
 				break;
-			} else {
-				System.out.println("數字格式不正確,請重新輸入");
-				sc.next();
 			}
+			System.out.println("數字格式不正確,請重新輸入");
+			sc.next();
 		}
+
 		System.out.println("欲格式化成(1)千分位(2)百分比(3)科學記號");
-		int s2 = sc.nextInt();
+		int s1 = sc.nextInt();
 
-		switch (s2) {
-
-		case 1:
-
-		case 2:
-
-		case 3:
+		if (s1 == 1) {
+			Format n1 = new DecimalFormat("#,###");
+			System.out.println("千分位: " + n1.format(number));
+		} else if (s1 == 2) {
+			Format n2 = new DecimalFormat("##%");
+			System.out.println("百分比: " + n2.format(number));
+		} else if (s1 == 3) {
+//			Format n3 = new DecimalFormat("0.0E0");
+//			System.out.println("科學記號: " + n3.format(number));
 		}
+
+//		switch (s2) {
+//
+//		case 1:
+//			Format n1 = new DecimalFormat("#,###");
+//			System.out.println(n1.format(number1));
+//		case 2:
+//			Format n2 = new DecimalFormat("##%");
+//			System.out.println(n2.format(number1));
+//		case 3:
+//			System.out.printf("科學記號數字: %E",number1 );
 
 		sc.close();
 	}
